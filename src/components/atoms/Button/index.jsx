a503a42c597a5b0cbec9845f509caa9626de5232
rohitsaicodes <rohitsaicodes@gmail.com>
2022-04-19 15:21:48 +0530

    NavBar changes

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -3,7 +3,11 @@ import { Button as MuiButton } from "@mui/material";
 
 const Button = (props) => {
     return (
-        <MuiButton variant={props.variant} onClick={props.onClick}>
+        <MuiButton
+            variant={props.variant}
+            onClick={props.onClick}
+            className={props.className}
+        >
             {props.name}
         </MuiButton>
     );
